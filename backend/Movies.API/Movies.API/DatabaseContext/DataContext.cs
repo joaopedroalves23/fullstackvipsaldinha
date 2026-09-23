8bafddeb3fa83d70beb390ce79888ca30ea1d7db
joaopedroalves23 <joaopedroalves230807@gmail.com>
2026-09-23 11:14:57 -0300

    add datacontext

---
+++ backend/Movies.API/Movies.API/DatabaseContext/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Mappings;
+using Movies.API.Models;
+
+namespace Movies.API.DatabaseContext;
+
+public class DataContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserMap());
+        modelBuilder.ApplyConfiguration(new MovieMap());
+    }
+}
